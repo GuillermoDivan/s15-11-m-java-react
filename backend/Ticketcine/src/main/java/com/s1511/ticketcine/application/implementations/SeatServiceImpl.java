27e5c943fc827c7f77ea4a8498ef5a550bc69fe6
Alex David Frías <adavidfruiz@gmail.com>
2024-06-01 04:29:38 -0500

    feat: add findReservedSeatsByUserId and releaseReservedSeats

--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/application/implementations/SeatServiceImpl.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/application/implementations/SeatServiceImpl.java
@@ -1,8 +1,11 @@
 package com.s1511.ticketcine.application.implementations;
 
+import com.s1511.ticketcine.application.dto.seat.SeatReleaseRequestDTO;
 import com.s1511.ticketcine.domain.entities.User;
 import com.s1511.ticketcine.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +16,7 @@ import com.s1511.ticketcine.domain.entities.Seat;
 import com.s1511.ticketcine.domain.repository.SeatRepository;
 import com.s1511.ticketcine.domain.services.SeatService;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,6 +28,7 @@ public class SeatServiceImpl implements SeatService {
     private final SeatRepository seatRepository;
     private final UserRepository userRepository;
     private final SeatMapper seatMapper;
+    private static final Logger logger = LoggerFactory.getLogger(SeatServiceImpl.class);
 
     @Override
     public List<SeatDTO> findAllSeats() {
@@ -38,6 +43,15 @@ public class SeatServiceImpl implements SeatService {
         Seat seat = seatRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Seat not found"));
         return seatMapper.toDTO(seat);
+    }
+
+    @Override
+    public List<SeatDTO> findReservedSeatsByUserId(String userId) {
+        List<Seat.Availability> availabilities = Arrays.asList(Seat.Availability.OCCUPIED, Seat.Availability.RESERVED);
+        List<Seat> reservedSeats = seatRepository.findByCurrentUser_IdAndAvailability(userId, availabilities);
+        return reservedSeats.stream()
+                .map(seatMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -66,6 +80,35 @@ public class SeatServiceImpl implements SeatService {
         seatRepository.save(seat);
         return Optional.of(seat); // Estado del asiento actualizado exitosamente
     }
+
+    @Override
+    @Transactional
+    public boolean releaseReservedSeats(SeatReleaseRequestDTO seatReleaseRequestDTO) {
+        Optional<User> userOptional = userRepository.findById(seatReleaseRequestDTO.userId());
+
+        if (!userOptional.isPresent()) {
+            logger.error("Usuario no encontrado o no activo: {}", seatReleaseRequestDTO.userId());
+            return false; // Usuario no encontrado o no activo
+        }
+
+        User user = userOptional.get();
+        List<Seat> seats = seatRepository.findAllById(seatReleaseRequestDTO.seatIds());
+
+        for (Seat seat : seats) {
+            if (seat.getCurrentUser().equals(user) && seat.getAvailability() == Seat.Availability.OCCUPIED) {
+                logger.info("Liberando asiento: {}, para el usuario: {}", seat.getId(), user.getId());
+                seat.setAvailability(Seat.Availability.RESERVED);
+                seatRepository.save(seat);
+            } else {
+                logger.warn("Asiento no puede ser liberado: {}, current user: {}, expected user: {}, availability: {}",
+                        seat.getId(), seat.getCurrentUser() != null ? seat.getCurrentUser().getId() : "null", user.getId(), seat.getAvailability());
+            }
+        }
+
+        return true;
+
+    }
+
 
 
 }
--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/services/SeatService.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/services/SeatService.java
@@ -1,5 +1,6 @@
 package com.s1511.ticketcine.domain.services;
 
+import com.s1511.ticketcine.application.dto.seat.SeatReleaseRequestDTO;
 import org.springframework.stereotype.Service;
 
 import com.s1511.ticketcine.application.dto.seat.SeatDTO;
@@ -14,6 +15,8 @@ public interface SeatService {
 
     List<SeatDTO> findAllSeats();
     SeatDTO findSeatById(String id);
+    List<SeatDTO> findReservedSeatsByUserId(String userId);
     Optional<Seat> seatReservation(String id, SeatReservationDTO seatReservationDTO);
+    boolean releaseReservedSeats(SeatReleaseRequestDTO seatReleaseRequestDTO);
 
 }
