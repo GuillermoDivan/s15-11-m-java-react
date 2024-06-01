b9c923d1e0a28c036ab52e7052f911fc0bca6964
Alex David Frías <adavidfruiz@gmail.com>
2024-06-01 04:29:10 -0500

    feat: add findByCurrentUser_IdAndAvailability

--- backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/repository/SeatRepository.java
+++ backend/Ticketcine/src/main/java/com/s1511/ticketcine/domain/repository/SeatRepository.java
@@ -1,16 +1,18 @@
 package com.s1511.ticketcine.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.s1511.ticketcine.domain.entities.Seat;
-import com.s1511.ticketcine.domain.entities.User;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, String> {
+    @Query("SELECT s FROM Seat s WHERE s.currentUser.Id = ?1 AND s.availability IN ?2")
+    List<Seat> findByCurrentUser_IdAndAvailability(String userId, List<Seat.Availability> availabilities);
     List<Seat> findByCinemaIdAndScreenId(String idCinema, String idScreen);
     List<Seat> findByScreenId(String idScreen);
     Optional<Seat> findBySeatNumberAndReserved(String seatNumber, Boolean reserved);
